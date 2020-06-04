2d66eb9fdfef7901012660e70b703d4d28b48594
Khasan Mukhammetrahimov <damskiy.ugodnik1997@gmail.com>
2020-06-04 19:20:00 +0600

    Первый комит

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var money = 250;
+let income = 10;
+const addExpenses = 100;
+var deposit = '50% скидки ,дествуют до 2 мая не упусти момент приобрести новую модель "Mi Band-5" ';
+const mission = "Написать переменную и вывести в консоль!";
+let period = "2 second";
+
+
+console.log(mission);
+alert(deposit);
+
+
+
+document.getElementById('github').onclick = function () {
+  console.log('Изучил git и github');
+};
