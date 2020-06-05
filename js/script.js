e7d4976a5ac7bd228ef1d55a897ab9262ea6d7c7
Khasan Mukhammetrahimov <damskiy.ugodnik1997@gmail.com>
2020-06-05 18:55:36 +0600

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-let money ;
-let income ;
-let addExpenses ;
-let deposit ;
-let mission ;
-let period ;
+let money = "700$";   //“Доход за месяц”
+console.log(money + " Доход за месяц");
+let income ='Курьер';       //строка с дополнительными доходом(например: фриланс),
+console.log(income + ' Подработка');
+let addExpenses ='Такси, Проживание, Питание, Канцелярские предметы, '; //строка с перечислением дополнительных расходов через запятую(например: интернет, такси, коммуналка)
+console.log(addExpenses.length);
+console.log(addExpenses.toLocaleLowerCase());
+console.log(addExpenses.split(', '));
+let deposit = true;//любое булево значение,
+console.log(deposit);
 
+let mission = 5000 ; //любое число(Какую сумму хотите накопить),
 
-console.log('Жизнь боль когда в Java Scripte ноль');
-alert('Just do it');
+let period = 5;//число от 1 до 12(месяцев)
+
+console.log( mission + ' евро, за ' + period  + ' месяцев');
+
+let budgetDay = '500$';
+console.log(budgetDay);
 
 
 
